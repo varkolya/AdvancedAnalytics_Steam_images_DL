d35a3e988d3675a557b47168827cb4703d40ef9e
voira <89135019+voira@users.noreply.github.com>
2024-05-29 17:07:12 +0200

    adj

--- check.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-# Check if CUDA is available
-if torch.cuda.is_available():
-    print("CUDA is available! You can use GPU acceleration.")
-else:
-    print("CUDA is not available. You can only use CPU for computation.")
